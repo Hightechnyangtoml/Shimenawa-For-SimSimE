f0cf525cfc7c5607a8e45d9bff1a70bcb55c3ca4
Theta <ddooriddoori763@gmail.com>
2022-11-06 17:47:32 +0900

    Yee

---
+++ src/main/java/kr/cafemoca/shimenawa/Shimenawa.java
@@ -0,0 +1,17 @@
+package kr.cafemoca.shimenawa;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Shimenawa extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
