a1f442f7c9af5502b2a4c08a9e5c4104d7c6f287
Theta <ddooriddoori763@gmail.com>
2022-11-07 18:35:33 +0900

    Workinnggggggg why text appear twicee

--- src/main/java/kr/cafemoca/shimenawa/Shimenawa.java
+++ src/main/java/kr/cafemoca/shimenawa/Shimenawa.java
@@ -7,30 +7,31 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Shimenawa extends JavaPlugin implements Listener {
 
 	@Override
 	public void onEnable() {
-		System.out.println("§cShimenawa§f For Neko initialized");
+		System.out.println("Shimenawa For Neko initialized");
 		Bukkit.getPluginManager().registerEvents(this, this);
 	}
 
 	@Override
 	public void onDisable() {}
 
-	private static final Material[] swords = new Material[] {
-			Material.NETHERITE_SWORD, Material.DIAMOND_SWORD, Material.GOLDEN_SWORD, Material.IRON_SWORD, Material.STONE_SWORD, Material.WOODEN_SWORD
-	};
+	private boolean isSword(ItemStack is) {
+		return is.getType().name().contains("SWORD");
+	}
 
 	@EventHandler
-	public void onChoponpoppy(PlayerInteractEvent e) { // not workinggggggg
+	public void onChoponpoppy(PlayerInteractEvent e) { // now working but why text print out twice???
 		Player p = e.getPlayer();
 		if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
 			if (e.getClickedBlock().getType() == Material.POPPY) {
-				if (e.getHand().equals(swords)) {
-					p.sendMessage("Right Clicked by types of sword");
+				if (isSword(p.getItemInHand())) {
+					p.sendMessage("Right Clicked by types of swords on Poppy");
 				}
 			}
 		}
